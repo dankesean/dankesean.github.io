ca2d5df1ee6184b9a2e863645334bdcf61158be3
Sean McGowan <sean.mcgowan@adelaide.edu.au>
2025-11-12 11:47:58 +1030

    Update random-gif.js

--- assets/js/random-gif.js
+++ assets/js/random-gif.js
@@ -1,15 +1,15 @@
-console.log("Random GIF script loaded!");
-
 document.addEventListener("DOMContentLoaded", function() {
+  console.log("Random GIF script loaded!"); // confirm script runs
+
   const gifs = [
-    "1.gif", "2.gif", "3.gif", "4.gif",
-    "5.gif", "6.gif", "7.gif", "8.gif"
+    "1.gif","2.gif","3.gif","4.gif","5.gif","6.gif","7.gif","8.gif"
   ];
 
   const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
   const gifElement = document.getElementById("profile-gif");
 
   if (gifElement) {
-    gifElement.src = "{{ '/images/' | relative_url }}" + randomGif;
+    // Use direct path that works on GitHub Pages
+    gifElement.src = "/images/" + randomGif;
   }
 });
